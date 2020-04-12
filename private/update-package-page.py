a3a390c68e43c81c3192dd1fd064bb43dc2d2c43
Sverrir Magnússon <sverrir@tutanota.com>
2020-04-12 13:10:05 +0000

    update-package-page.py complete

--- private/update-package-page.py
+++ private/update-package-page.py
@@ -8,6 +8,9 @@ if __name__ == "__main__":
     package_name: str = sys.argv[1]
     package_version: str = sys.argv[2]
     package_actual_name: str = sys.argv[3]
+    package_project_name: str = sys.argv[4]
+    package_source_directory: str = sys.argv[5]
+
 
     with open("./" + package_name + "/versions.json", "r+") as f:
         versions_json = json.load(f)
@@ -15,30 +18,30 @@ if __name__ == "__main__":
         if package_version in versions_json:
             print("version has already been uploaded", file=sys.stderr)
             exit(1)
-        packages_json[package_version] = f"<a href=\"git+https://gitlab.com/zigseg/iot-core/-/tree/develop/python/ModuleFramework#egg={package_actual_name}-{package_version}\">{package_actual_name}-{package_version}</a><br/>"
+        sub_directory: str = "" if package_source_directory == "." else f"subdirectory={package_source_directory}&"
+        versions_json[package_version] = f"<a href=\"git+ssh://git@gitlab.com/{package_project_name}@{package_version}{sub_directory}#egg={package_actual_name}-{package_version}\">{package_actual_name}-{package_version}</a>"
+        f.write(json.dumps(versions_json))
     
-    folder_structure_html = ""
-    
-    packages: List[str] = packages_json["packages"]
-    packages.sort()
+    version_list_html: str = ""
 
-    for package in packages:
-        folder_structure_html += """<div class="package" >
-	    <a href=\"""" + package + "/\">" + package + """</a>
-	</div>
+    for version in versions_json:
+        version_list_html += """  <li>
+	    """ + versions_json[version] + """
+      </li>
         """
 
     page_html: str = """<!DOCTYPE html>
 <html>
   <head>
-    <title>Links for python_world</title>
+    <title>Links for """ + package_actual_name + """</title>
   </head>
   <body>
-    <h1>Links for """+ python_world + """</h1>
-    """ + links +
-  """
+    <h1>Links for """+ package_actual_name + """</h1>
+    <ul>
+    """ + version_list_html + """
+    </ul>
   </body>
 </html>"""
-
+    print(page_html)
     with open("./" + package_name + "/index.html", "w") as f:
         f.write(page_html)
